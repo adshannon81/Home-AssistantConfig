323a36d468da638b71b934fd87f89bdf2f729821
adshannon81 <ad.shannon@gmail.com>
2018-07-10 09:14:02 +0000

    update to IP Checker

--- IPChecker.sh
+++ IPChecker.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
 
-DATE=`date '+%Y-%m-%d %H:%M:%S'` >>IPChecker.log
-echo "-----------------------------------------" >> IPChecker.log
-echo "| IP Checker - ${DATE} " >> IPChecker.log
-echo "-----------------------------------------" >> IPChecker.log
+DATE=`date '+%Y-%m-%d %H:%M:%S'` 
+echo "-----------------------------------------" 
+echo "| IP Checker - ${DATE} " 
+echo "-----------------------------------------" 
 
 restartHA="0"
 
-echo "RM Kitchen"  >> IPChecker.log
+echo "RM Kitchen"  
 currentIP=$(</home/homeassistant/.homeassistant/IP/rmKitchenIP.txt)
-echo ".$currentIP." >> IPChecker.log
+echo ".$currentIP." 
 
 sudo cp /home/homeassistant/.homeassistant/configuration.yaml /home/homeassistant/.homeassistant/configuration.yaml.backup
 sudo cp /home/homeassistant/.homeassistant/scripts.yaml /home/homeassistant/.homeassistant/scripts.yaml.backup
 
 newIP=`sudo nmap -sP --disable-arp-ping 192.168.1.0/24 >/dev/null && arp -an | grep 34:ea:34:58:a9:67 | awk '{print $2}' | sed 's/[()]//g'`
-echo ".$newIP." >> IPChecker.log
+echo ".$newIP." 
 
 if [[ "$newIP" != "" ]]; then
  if [[ "$newIP" == "$currentIP" ]]; then
-  echo "equal" >> IPChecker.log
+  echo "equal" 
  else
-  echo " not equal" >> IPChecker.log
+  echo " not equal"
   sudo sed -i -e "s/${currentIP}/${newIP}/g" /home/homeassistant/.homeassistant/IP/rmKitchenIP.txt
   sudo sed -i -e "s/${currentIP}/${newIP}/g" /home/homeassistant/.homeassistant/configuration.yaml
   currentIP2=$( echo "$currentIP" | tr . _ )
@@ -31,23 +32,23 @@ if [[ "$newIP" != "" ]]; then
   restartHA="1"
  fi
 else
-  echo "IP not found" >> IPChecker.log
+  echo "IP not found" 
 fi
 
 
 
-echo "RM Pro" >> IPChecker.log
+echo "RM Pro" 
 currentIP=$(</home/homeassistant/.homeassistant/IP/rmProIP.txt)
-echo ".$currentIP." >> IPChecker.log
+echo ".$currentIP." 
 
 newIP=`sudo nmap -sP --disable-arp-ping 192.168.1.0/24 >/dev/null && arp -an | grep b4:43:0d:fc:1e:71 | awk '{print $2}' | sed 's/[()]//g'`
-echo ".$newIP." >> IPChecker.log
+echo ".$newIP." 
  
 if [[ "$newIP"  != "" ]]; then 
  if [[ "$currentIP" == "$newIP" ]]; then
-  echo "equal" >> IPChecker.log
+  echo "equal" 
  else 
-  echo "not equal" >> IPChecker.log
+  echo "not equal" 
   sudo sed -i -e "s/${currentIP}/${newIP}/g" /home/homeassistant/.homeassistant/IP/rmProIP.txt  
   sudo sed -i -e "s/${currentIP}/${newIP}/g" /home/homeassistant/.homeassistant/configuration.yaml
   currentIP2=$(echo "$currentIP" | tr . _ )
@@ -56,14 +57,14 @@ if [[ "$newIP"  != "" ]]; then
   restartHA=1
  fi
 else
- echo "IP not found" >> IPChecker.log
+ echo "IP not found" 
 fi
 
 
 if [[ "$restartHA" == "1" ]]; then
- echo "restart = $restartHA" >> IPChecker.log
+ echo "restart = $restartHA" 
  sudo systemctl stop home-assistant@homeassistant
  sudo systemctl start home-assistant@homeassistant
 else
- echo "Restart not required" >> IPChecker.log
+ echo "Restart not required" 
 fi
